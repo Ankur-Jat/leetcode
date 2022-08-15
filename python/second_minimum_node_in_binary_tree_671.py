2b10f0da8443155b547702ca07e4a6822012c41a
Ankur Jat <ankurandroid2908@gmail.com>
2022-08-16 00:06:45 +0200

    test cases added for second minimum node in binary tree #671

--- python/second_minimum_node_in_binary_tree_671.py
+++ python/second_minimum_node_in_binary_tree_671.py
@@ -1,9 +1,11 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from collections import deque
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution(object):
     def findSecondMinimumValue(self, root):
         """
@@ -25,4 +27,21 @@ class Solution(object):
                 queue.append(node.left)
                 queue.append(node.right)
         return b if b != float('inf') else -1
-        
+
+
+def test():
+    solution = Solution()
+
+    root = TreeNode(1)
+    root.left = TreeNode(1)
+    root.right = TreeNode(2)
+    assert solution.findSecondMinimumValue(root) == 2, "First case wrong zigzagLevelOrder result"
+
+    root = TreeNode(2)
+    root.left = TreeNode(2)
+    root.right = TreeNode(2)
+    assert solution.findSecondMinimumValue(root) == -1, "Second case wrong zigzagLevelOrder result"
+
+
+if __name__ == "__main__":
+    test()
